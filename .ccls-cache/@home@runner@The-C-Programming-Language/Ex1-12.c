54d2ca356c25a12e9143ce621615980a3a1fa831
Ellis <48781372+ellispinsky@users.noreply.github.com>
2023-02-21 16:11:31 +0000

    rename

---
+++ .ccls-cache/@home@runner@The-C-Programming-Language/Ex1-12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* count lines in input */
+main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t' || c == '-')
+      putchar('\n');
+    else
+      putchar(c);
+  }
+}
